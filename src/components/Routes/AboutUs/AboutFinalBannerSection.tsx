72839e74ec443ed97ccc4836fbd8896da279c494
mugisham37 <mugisham333@gmail.com>
2025-12-25 22:12:05 +0200

    Aboutfinalbannersection

--- src/components/Routes/AboutUs/AboutFinalBannerSection.tsx
+++ src/components/Routes/AboutUs/AboutFinalBannerSection.tsx
@@ -1,8 +1,116 @@
-import React from 'react'
+import Image from 'next/image'
 
 const AboutFinalBannerSection = () => {
   return (
-    <div>AboutFinalBannerSection</div>
+    <div id="shopify-section-template--26430796890455__47daf102-ce31-4b33-906a-eae624818962"
+      className="relative">
+      <section className="relative pt-0 pb-2.5 lg:pb-5 -mb-3.5 rounded-b-2xl bg-[#e5e2de]">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="flex flex-col">
+            <div className="w-full">
+              <div className="relative">
+                <Image 
+                  src="//www.notsellingliquid.com/cdn/shop/files/NSL_Interieur_HR-1273.jpg?v=1759500048" 
+                  alt=""
+                  loading="lazy" 
+                  className="w-full h-auto rounded-lg hidden lg:block" 
+                  width={3000}
+                  height={2001}
+                  unoptimized
+                />
+                <Image
+                  src="//www.notsellingliquid.com/cdn/shop/files/NSL_Interieur_HR-1273.jpg?v=1759500048"
+                  alt="" 
+                  loading="lazy" 
+                  className="w-full h-auto rounded-lg block lg:hidden" 
+                  width={3000}
+                  height={2001}
+                  unoptimized
+                />
+                <div className="absolute inset-0 flex items-start justify-start">
+                  <div className=""></div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="relative">
+                <Image 
+                  src="//www.notsellingliquid.com/cdn/shop/files/NSL_Interieur_HR-1257.jpg?v=1759500048" 
+                  alt=""
+                  loading="lazy" 
+                  className="w-full h-auto rounded-lg hidden lg:block" 
+                  width={3000}
+                  height={2001}
+                  unoptimized
+                />
+                <Image
+                  src="//www.notsellingliquid.com/cdn/shop/files/NSL_Interieur_HR-1257.jpg?v=1759500048"
+                  alt="" 
+                  loading="lazy" 
+                  className="w-full h-auto rounded-lg block lg:hidden" 
+                  width={3000}
+                  height={2001}
+                  unoptimized
+                />
+                <div className="absolute inset-0 flex items-start justify-start">
+                  <div className=""></div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="relative">
+                <Image 
+                  src="//www.notsellingliquid.com/cdn/shop/files/NSL_Interieur_HR-1364.jpg?v=1759500046" 
+                  alt=""
+                  loading="lazy" 
+                  className="w-full h-auto rounded-lg hidden lg:block" 
+                  width={3000}
+                  height={2001}
+                  unoptimized
+                />
+                <Image
+                  src="//www.notsellingliquid.com/cdn/shop/files/NSL_Interieur_HR-1364.jpg?v=1759500046"
+                  alt="" 
+                  loading="lazy" 
+                  className="w-full h-auto rounded-lg block lg:hidden" 
+                  width={3000}
+                  height={2001}
+                  unoptimized
+                />
+                <div className="absolute inset-0 flex items-start justify-start">
+                  <div className=""></div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="relative">
+                <Image 
+                  src="//www.notsellingliquid.com/cdn/shop/files/NSL_Interieur_HR-1334.jpg?v=1759500047" 
+                  alt=""
+                  loading="lazy" 
+                  className="w-full h-auto rounded-lg hidden lg:block" 
+                  width={3000}
+                  height={2001}
+                  unoptimized
+                />
+                <Image
+                  src="//www.notsellingliquid.com/cdn/shop/files/NSL_Interieur_HR-1334.jpg?v=1759500047"
+                  alt="" 
+                  loading="lazy" 
+                  className="w-full h-auto rounded-lg block lg:hidden" 
+                  width={3000}
+                  height={2001}
+                  unoptimized
+                />
+                <div className="absolute inset-0 flex items-start justify-start">
+                  <div className=""></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
