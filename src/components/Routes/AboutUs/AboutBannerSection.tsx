4793583847814550107c465a907ee1eb6324f761
mugisham37 <mugisham333@gmail.com>
2025-12-25 20:29:28 +0200

    Migrated the about banner section

--- src/components/Routes/AboutUs/AboutBannerSection.tsx
+++ src/components/Routes/AboutUs/AboutBannerSection.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+// Self-contained Tailwind component - no external CSS dependencies required
+// Converted from: //www.notsellingliquid.com/cdn/shop/t/39/assets/banner.css?v=54277540370872013631758090047
 
 const AboutBannerSection = () => {
   return (
-    <div>AboutBannerSection</div>
+    <div 
+      id="shopify-section-template--26430796890455__809c3410-f45b-4408-a2c4-a969ad86ae98"
+      className="shopify-section main_banner"
+    >
+      <section
+        className="section-banner section-id-template--26430796890455__809c3410-f45b-4408-a2c4-a969ad86ae98 relative-sec relative pt-2.5 lg:pt-5 pb-0 -mt-[15px] rounded-t-[15px] bg-[#f7f7f6]"
+      >
+        <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="banner-wrap [&_img]:rounded-lg [&_video]:rounded-lg"></div>
+        </div>
+      </section>
+    </div>
   )
 }
 
