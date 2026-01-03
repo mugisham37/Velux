ec07e5c3a8ce507186a6266584d8da33257fe5b2
mugisham37 <mugisham333@gmail.com>
2026-01-03 13:21:41 +0200

    Made the about intro section self contained

--- src/components/Routes/AboutUs/AboutIntroSection.tsx
+++ src/components/Routes/AboutUs/AboutIntroSection.tsx
@@ -3,56 +3,80 @@
 
 const AboutIntroSection = () => {
   return (
-    <div id="shopify-section-template--26430796890455__e888cf0f-1c90-47ca-a12b-c0a1b957457e"
-                className="shopify-section about_intro">
-                <link
-                    href="//www.notsellingliquid.com/cdn/shop/t/39/assets/about-intro.css?v=23907028594662014411758090047"
-                    rel="stylesheet" type="text/css" media="all" />
-                <section
-                    className="section-about-intro section-id-template--26430796890455__e888cf0f-1c90-47ca-a12b-c0a1b957457e relative-sec">
-                    <style>
-                        {`.section-id-template--26430796890455__e888cf0f-1c90-47ca-a12b-c0a1b957457e {
-                            background: #262424;
-                        }`}
-                    </style>
-                    <div className="container">
-                        <div className="about-intro grid">
-                            <div className="ai-left grid-6-fcol">
-                                <div className="ai-leftinr">
-                                    <p className="n-body">About us</p>
-                                    <h2>E-commerce is our language, fashion is our culture.</h2>
-                                </div>
-                            </div>
-                            <div className="ai-right grid-4-lcol">
-                                <div className="ai-rightinr">
-                                    <div className="s-body d-none d-lg-block">We&apos;ve taken things we&apos;re passionate about -
-                                        fashion, sneakers and all things digital - and made it our craft.</div>
-                                    <div className="s-caption">
-                                        <p>We&apos;re creating a space where e-commerce expertise is combined with an
-                                            intrinsic fashion mindset to grow next generation brands in the digital
-                                            landscape.</p>
-                                    </div><a href="/pages/manifesto" className="link-arrow">
-                                        <span className="arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                height="16" viewBox="0 0 16 16" fill="none">
-                                                <path fillRule="evenodd" clipRule="evenodd"
-                                                    d="M8.375 2.79297L13.5821 8.00008L8.375 13.2072L7.66789 12.5001L11.6679 8.50008H2.625V7.50008H11.6679L7.66789 3.50008L8.375 2.79297Z"
-                                                    fill="initial"></path>
-                                            </svg></span>
-                                        <span className="label">Read full manifesto</span>
-                                        <span className="arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                height="16" viewBox="0 0 16 16" fill="none">
-                                                <path fillRule="evenodd" clipRule="evenodd"
-                                                    d="M8.375 2.79297L13.5821 8.00008L8.375 13.2072L7.66789 12.5001L11.6679 8.50008H2.625V7.50008H11.6679L7.66789 3.50008L8.375 2.79297Z"
-                                                    fill="initial"></path>
-                                            </svg></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="intro_overly">&nbsp;</div>
-                    </div>
-                </section>
+    <div className="block">
+      <section className="relative bg-[#262424] py-16 lg:py-20">
+        <div className="max-w-[1200px] mx-auto px-5 lg:px-10">
+          <div className="flex flex-col lg:flex-row lg:gap-8">
+            {/* Left Column - 6/12 grid */}
+            <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
+              <div className="lg:pr-8">
+                <p 
+                  className="text-white text-xs uppercase tracking-wider mb-4 lg:mb-6"
+                  style={{ fontFamily: 'Aeonik, sans-serif' }}
+                >
+                  About us
+                </p>
+                <h2 
+                  className="text-white text-[40px] lg:text-[60px] leading-[1.1] font-light"
+                  style={{ fontFamily: 'PP Editorial Old, serif' }}
+                >
+                  E-commerce is our language, fashion is our culture.
+                </h2>
+              </div>
             </div>
+            
+            {/* Right Column - 4/12 grid */}
+            <div className="w-full lg:w-5/12 lg:ml-auto">
+              <div className="lg:pl-4">
+                <div 
+                  className="hidden lg:block text-white text-sm lg:text-base mb-6 leading-relaxed"
+                  style={{ fontFamily: 'Aeonik, sans-serif' }}
+                >
+                  We&apos;ve taken things we&apos;re passionate about - fashion, sneakers and all things digital - and made it our craft.
+                </div>
+                <div className="mb-8">
+                  <p 
+                    className="text-white text-xs lg:text-sm leading-relaxed"
+                    style={{ fontFamily: 'Aeonik, sans-serif' }}
+                  >
+                    We&apos;re creating a space where e-commerce expertise is combined with an intrinsic fashion mindset to grow next generation brands in the digital landscape.
+                  </p>
+                </div>
+                <a 
+                  href="/pages/manifesto" 
+                  className="inline-flex items-center gap-2 text-white text-xs lg:text-sm hover:opacity-80 transition-opacity group"
+                  style={{ fontFamily: 'Aeonik, sans-serif' }}
+                >
+                  <span className="transform group-hover:translate-x-1 transition-transform">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                      <path 
+                        fillRule="evenodd" 
+                        clipRule="evenodd"
+                        d="M8.375 2.79297L13.5821 8.00008L8.375 13.2072L7.66789 12.5001L11.6679 8.50008H2.625V7.50008H11.6679L7.66789 3.50008L8.375 2.79297Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </span>
+                  <span>Read full manifesto</span>
+                  <span className="transform group-hover:translate-x-1 transition-transform">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                      <path 
+                        fillRule="evenodd" 
+                        clipRule="evenodd"
+                        d="M8.375 2.79297L13.5821 8.00008L8.375 13.2072L7.66789 12.5001L11.6679 8.50008H2.625V7.50008H11.6679L7.66789 3.50008L8.375 2.79297Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Overlay element */}
+          <div className="absolute inset-0 pointer-events-none">&nbsp;</div>
+        </div>
+      </section>
+    </div>
   )
 }
 
