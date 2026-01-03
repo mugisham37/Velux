4da01a0f54981210495dc3aa99303084c1ffe3c7
mugisham37 <mugisham333@gmail.com>
2026-01-03 15:03:00 +0200

    Made the about intro section look how i wanted it to look

--- src/components/Routes/AboutUs/AboutIntroSection.tsx
+++ src/components/Routes/AboutUs/AboutIntroSection.tsx
@@ -1,14 +1,11 @@
-// Self-contained Tailwind component - no external CSS dependencies required
-// Converted from: https://www.notsellingliquid.com/cdn/shop/t/39/assets/about-intro.css?v=23907028594662014411758090047
-
 const AboutIntroSection = () => {
   return (
-    <div className="block">
-      <section className="relative bg-[#262424] py-16 lg:py-20">
-        <div className="max-w-[1200px] mx-auto px-5 lg:px-10">
+    <div className="block min-w-screen">
+      <section className="relative bg-[#262424] pt-60 pb-10 lg:pb-12 lg:pt-100">
+        <div className="mx-auto px-4 lg:px-4">
           <div className="flex flex-col lg:flex-row lg:gap-8">
             {/* Left Column - 6/12 grid */}
-            <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
+            <div className="w-full lg:w-1/2 mb-4 lg:mb-0">
               <div className="lg:pr-8">
                 <p 
                   className="text-white text-xs uppercase tracking-wider mb-4 lg:mb-6"
@@ -26,17 +23,17 @@ const AboutIntroSection = () => {
             </div>
             
             {/* Right Column - 4/12 grid */}
-            <div className="w-full lg:w-5/12 lg:ml-auto">
+            <div className="w-full lg:w-5/12 lg:ml-auto lg:border-l lg:border-white lg:pl-4">
               <div className="lg:pl-4">
                 <div 
-                  className="hidden lg:block text-white text-sm lg:text-base mb-6 leading-relaxed"
+                  className="hidden lg:block text-white text-sm lg:text-base mb-6 leading-relaxed uppercase"
                   style={{ fontFamily: 'Aeonik, sans-serif' }}
                 >
                   We&apos;ve taken things we&apos;re passionate about - fashion, sneakers and all things digital - and made it our craft.
                 </div>
                 <div className="mb-8">
                   <p 
-                    className="text-white text-xs lg:text-sm leading-relaxed"
+                    className="text-white text-sm lg:text-sm leading-relaxed"
                     style={{ fontFamily: 'Aeonik, sans-serif' }}
                   >
                     We&apos;re creating a space where e-commerce expertise is combined with an intrinsic fashion mindset to grow next generation brands in the digital landscape.
@@ -57,7 +54,7 @@ const AboutIntroSection = () => {
                       />
                     </svg>
                   </span>
-                  <span>Read full manifesto</span>
+                  <span className="uppercase">Read full manifesto</span>
                   <span className="transform group-hover:translate-x-1 transition-transform">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                       <path 
